c4ffee0aea2257392c1997c9a8c998bff831327f
Raghav Kapoor <raghavkapoor16947@gmail.com>
2025-02-04 00:11:31 +0530

    Update base_url.js

--- src/base_url.js
+++ src/base_url.js
@@ -1,3 +1,4 @@
-const baseUrl = "https://e-commerce-backend-production-bffa.up.railway.app/"
+// const baseUrl = "https://e-commerce-backend-production-bffa.up.railway.app/"
+const baseUrl = "https://e-commerce-backend-production-0c3d.up.railway.app"
 
 export default baseUrl
